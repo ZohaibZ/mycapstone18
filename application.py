5268faf1539de89b9b3b07ded81d5f320ddf97d8
Zohaib <zohaib.zahid.18@gmail.com>
2018-04-19 15:56:07 -0400

    fix fix fix

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import create_engine, MetaData, Table
+from application import db, application
+from application.models import *
+from application.forms import *
+
+application.secret_key = 'Capstone2018'
+
+@app.route('/index.html')
+@app.route('/')
+def index():
+    engine = create_engine('mysql+pymysql://Capstone2018:capstonehelp@capstone2018.cffs7lx8bqq2.us-east-1.rds.amazonaws.com:3306/Capstone2018', convert_unicode=True)
+    connection = engine.connect()
+
+    return render_template('index.html')
+
+@app.route('/pages/<folder>/<page>')
+def pages(folder,page):
+    X = 'pages/{}/{}'.format(folder,page)
+    print(X)
+    return render_template(X)
+
+if __name__== '__main__':
+# here is starting of the development HTTP server
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://Capstone2018:capstonehelp@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/Capstone2018'
+SQLALCHEMY_POOL_RECYCLE = 3600
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'Capstone2018'
